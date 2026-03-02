17b4c51b2a65cc6322ca3f666c2ca651c16dc297
akshaytheGodxo <akshaysinghc230@gmail.com>
2026-03-02 20:26:35 +0530

    more optimized

--- 416.cpp
+++ 416.cpp
@@ -8,30 +8,31 @@ public:
     bool canPartition(vector<int>& nums) {
         int sum = 0;
         for (auto i : nums) sum += i;
-        if (sum % 2 != 0) return false;
-
-        sum /= 2;
         int n=nums.size();
+        if (sum % 2 != 0) return false;
+        sum /= 2;
         bool dp[n+1][sum+1];
         dp[0][0] = true;
-        for (int i=1;i<n+1;i++) {
+        for (int i=1;i<=n;i++) {
             dp[i][0] = true;
         }
-        for (int i=1;i<sum+1;i++) {
+
+        for (int i=1;i<=sum;i++) {
             dp[0][i] = false;
-        } 
+        }
 
         for (int i=1;i<=n;i++) {
             for (int j=1;j<=sum;j++) {
                 dp[i][j] = dp[i-1][j];
+                // this condition is applied so that we can check, when the value of sum tracker is finally ready to include the previous num inside it, here j is the sum tracker
                 if (j >= nums[i-1]) {
-                    dp[i][j] = (dp[i][j] || dp[i-1][j-nums[i-1]]);
+                    dp[i][j] = max(dp[i-1][j], dp[i-1][j - nums[i-1]]);
                 }
             }
         }
 
         return dp[n][sum];
-    }
+    } 
 };
 
 int main(int argc, char const *argv[])
