0ccb67f404224f2918690a98e9539607ad8998e3
akshaytheGodxo <akshaysinghc230@gmail.com>
2026-04-03 23:02:35 +0530

    more questioons done

--- 30.cpp
+++ 30.cpp
@@ -4,7 +4,27 @@ class Solution
 {
 public:
     vector<int> findSubstring(string s, vector<string> &words)    {
-        
+        map<string, int> count;
+        map<string, int> curr;
+        int maxi = INT_MIN;
+        for (auto i:words) {
+            count[i]++;
+            maxi = max(maxi, i.size());
+        }
+        int n=s.size();
+        string last = "";
+        for (int i=0;i<n;i++) {
+            last += s[i];
+            auto it = count.find(last);
+            if (it != count.end()) {
+                
+            } else {
+                if (last.size() >= maxi) {
+                    last = "";
+                }
+            }
+        }
+
     }
 };
 int main()
